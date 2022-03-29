4ad00f98bbcd18c07d8b102ee4b8f6fad007c56d
MuruganVelu <muruganrvelu@gmail.com>
2022-03-29 08:57:20 +0530

    first commit

---
+++ MavenTest/src/main/java/MavenTest/MavenTest/MavenJenkinTest.java
@@ -0,0 +1,31 @@
+package MavenTest.MavenTest;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class MavenJenkinTest {
+	@Test
+	public void keyBoardAction() throws Exception{
+		
+		System.setProperty("webdriver.chrome.driver", "G:\\drivers\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		driver.get("https://www.amazon.in/");
+		System.out.println("URL Launched");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		WebElement element = driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']"));
+		Actions act = new Actions(driver);
+		act.moveToElement(element).click().keyDown(Keys.SHIFT).sendKeys("apple").build().perform();
+		
+	
+	}
+
+}
